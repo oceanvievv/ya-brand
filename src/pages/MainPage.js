f147b00a9ba3c9d2f43aded60a99ef56b6683365
oceanvievv <oceanvievv@protonmail.com>
2022-08-31 20:08:06 +0500

    Move Subscribe component to base folder

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -2,7 +2,7 @@ import { Promo } from "../components/MainPage/Promo/Promo";
 import { Categories } from "../components/MainPage/Categories/Categories";
 import Featured from "../components/MainPage/Featured/Featured";
 import Advantages from "../components/MainPage/Advantages/Advantages";
-import Subscribe from "../components/MainPage/Subscribe/Subscribe";
+import Subscribe from "../components/base/Subscribe/Subscribe";
 
 export function MainPage() {
   return (
