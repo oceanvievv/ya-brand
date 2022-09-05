b12a638b4d1314e8505380d49caf461bacd4d655
oceanvievv <oceanvievv@protonmail.com>
2022-09-05 22:52:10 +0500

    Add FetchErr component

---
+++ src/components/base/FetchErr/FetchErr.js
@@ -0,0 +1,14 @@
+import "./css/fetch-err.scss";
+
+export default function FetchErr({ fetchType, fetchErr }) {
+  return (
+    <div className="fetch-err-wrapper">
+      <span className="fetch-err__type md:fetch-err__type_md">
+        FAILED TO FETCH {fetchType}
+      </span>
+      <span className="fetch-err__err md:fetch-err__err_md">
+        ⚠ &nbsp; {fetchErr ? fetchErr : null} &nbsp; ⚠
+      </span>
+    </div>
+  );
+}
