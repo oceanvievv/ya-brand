9d7ce4d49bb373dce1dc0cac37a6fef702416f55
oceanvievv <oceanvievv@protonmail.com>
2022-09-05 22:53:20 +0500

    Use Loader and FetchErr components in Featured components

--- src/components/MainPage/Featured/Featured.js
+++ src/components/MainPage/Featured/Featured.js
@@ -2,12 +2,16 @@ import "./css/featured.scss";
 import ProductCard from "../../base/ProductCard/ProductCard";
 import { useEffect, useState } from "react";
 import { fetchFeaturedProducts } from "../../../data/data";
+import FetchErr from "../../base/FetchErr/FetchErr";
+import Loader from "../../base/Loader/Loader";
 
 export default function Featured() {
   const [products, setProducts] = useState([]);
+  const [isFetching, setIsFetching] = useState(true);
+  const [fetchErr, setFetchErr] = useState(null);
 
   useEffect(() => {
-    fetchFeaturedProducts(setProducts).then();
+    fetchFeaturedProducts(setProducts, setFetchErr, setIsFetching).then();
   }, []);
 
   return (
@@ -18,17 +22,27 @@ export default function Featured() {
           Shop for items based on what we featured in this week
         </div>
       </div>
-      <div className="featured-items md:featured-items_md">
-        {products.map((data, i) => (
-          <ProductCard key={i} data={data} />
-        ))}
-      </div>
-      <button
-        className="featured__show-more-btn"
-        onClick={() => console.log("srat")}
-      >
-        Browse All Products
-      </button>
+      {isFetching && <Loader />}
+      {!fetchErr && (
+        <>
+          <div className="featured-items md:featured-items_md">
+            {products.map((data, i) => (
+              <ProductCard key={i} data={data} />
+            ))}
+          </div>
+          {!isFetching && (
+            <button
+              className="featured__show-more-btn"
+              onClick={() => console.log("srat")}
+            >
+              Browse All Products
+            </button>
+          )}
+        </>
+      )}
+      {fetchErr && (
+        <FetchErr fetchType={"FEATURED PRODUCTS"} fetchErr={fetchErr} />
+      )}
     </div>
   );
 }
--- src/data/data.js
+++ src/data/data.js
@@ -1,16 +1,20 @@
 import client from "./client";
 
-async function fetchFeaturedProducts(setData) {
+async function fetchFeaturedProducts(setData, setFetchErr, setIsFetching) {
   let response;
 
   try {
     response = await client.get("products?limit=6");
   } catch (e) {
-    console.error(e);
+    console.error(e.message);
+    setFetchErr(e.message);
     response = { data: [] };
   }
 
-  setData(response.data);
+  setTimeout(() => {
+    setData(response.data);
+    setIsFetching(false);
+  }, 1000);
 }
 
 export { fetchFeaturedProducts };
